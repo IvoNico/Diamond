037013d3408d0dc47f530d5c1cf23602448a07d9
Ivo Nicolás Chapochnikoff <chapochnikoffivo@gmail.com>
2022-03-11 12:43:21 -0300

    creando NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function NavBar(){
+    return(
+        <header>
+            <nav className='navbar-container'>
+            <div>
+                <img className='navbar-logo' src="./assets/Diamond-logo.png" alt='Imagen de un diamante minimalista que representa a Diamond Bienes Raices'/>
+            </div>
+            <ul className='navbar-list'>
+                <li><Button className='list-button' variant="text" >Home</Button></li>
+                <li><Button className='list-button' variant="text">Productos</Button></li>
+                <li><Button className='list-button' variant="text">Nosotros</Button></li>
+                <li><Button className='list-button' variant="text">Contacto</Button></li>
+            </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
